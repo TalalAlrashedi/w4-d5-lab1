5f997fec218570c2b0b0d836ba78805d8ede0455
TalalAlrashedi <TalalAlrashedi24@hotmail.com>
2025-05-08 10:03:04 +0300

    upload

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-let intro = document.getElementById("intro")
-let changeText = document.getElementById("changeText")
-let changeSizeMore = document.getElementById("changeSizeMore")
-let changeSizeLess = document.getElementById("changeSizeLess")
-let changeColor = document.getElementById("changeColor")
-let changeBackground = document.getElementById("changeBackground")
-let ChangeImage = document.getElementById("ChangeImage")
+let intro = document.getElementById("intro");
+let changeText = document.getElementById("changeText");
+let changeSizeMore = document.getElementById("changeSizeMore");
+let changeSizeLess = document.getElementById("changeSizeLess");
+let changeColor = document.getElementById("changeColor");
+let changeBackground = document.getElementById("changeBackground");
+let ChangeImage = document.getElementById("ChangeImage");
 
-const changeTextF
+let text = "hello";
+let FontSize = 1;
+
+function changeTextFun() {
+  intro.innerText = text;
+}
+
+function changeSizeMoreFun() {
+  FontSize += 2 + "px";
+}
