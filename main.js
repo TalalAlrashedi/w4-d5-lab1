343b24b8eba20c4fc7f9175824eb7fc787a9bc49
TalalAlrashedi <TalalAlrashedi24@hotmail.com>
2025-05-08 09:52:43 +0300

    upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let intro = document.getElementById("intro")
+let changeText = document.getElementById("changeText")
+let changeSizeMore = document.getElementById("changeSizeMore")
+let changeSizeLess = document.getElementById("changeSizeLess")
+let changeColor = document.getElementById("changeColor")
+let changeBackground = document.getElementById("changeBackground")
+let ChangeImage = document.getElementById("ChangeImage")
+
+const changeTextF
